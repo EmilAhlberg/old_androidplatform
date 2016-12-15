b1320a8b53fa211822158c89350225491a12fa7e
Emil Ahlberg <emil.ahlberg1@gmail.com>
2016-12-15 13:21:01 +0100

    images!

--- src/main/java/Game/GameBlock.java
+++ src/main/java/Game/GameBlock.java
@@ -3,6 +3,10 @@ package Game;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
+
+import com.example.emil.app.R;
 
 /**
  * Created by Emil on 2016-11-22.
@@ -10,15 +14,19 @@ import android.graphics.Paint;
 
 public class GameBlock extends Block {
     protected static Paint paint = new Paint();
+    private Drawable picture;
 
     public GameBlock(Position position) {
         super(position);
         paint.setColor(Color.RED);
+        picture = board.getResources().getDrawable(R.drawable.block2);
     }
 
     @Override
     public void draw() {
-        canvas.drawRect((float)position.getX(),(float)position.getY(),(float)(position.getX()+width),(float)(position.getY()+height),paint);
+        //canvas.drawRect((float)position.getX(),(float)position.getY(),(float)(position.getX()+width),(float)(position.getY()+height),paint);
+        picture.setBounds((int) position.getX(), (int) position.getY(), (int) position.getX() +width, (int) position.getY() + height);
+        picture.draw(canvas);
     }
 
     @Override
